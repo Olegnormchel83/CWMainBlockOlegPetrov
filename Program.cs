e406421560604de6508ea9398ef43f48bfcc670b
Oleg OJ <cranberries2000@gmail.com>
2023-12-02 18:47:00 +0300

    Обновления алгоритма и красивый вывод

--- Program.cs
+++ Program.cs
@@ -8,22 +8,29 @@ using System.Threading.Tasks;
 string[] array_of_words_1 = {"Hello", "2", "world", ":-)"};
 string[] array_of_words_2 = {"1234", "1567", "-2", "computer science"};
 string[] array_of_words_3 = {"Russia", "Denmark", "Kazan"};
+var str_1 = string.Join(", ", array_of_words_1);
+var str_2 = string.Join(", ", array_of_words_2);
+var str_3 = string.Join(", ", array_of_words_3);
 
 void check_words_array(string[] arr)
 {
-    int count_words = 0, count_symbols = 0;
-
-    Console.WriteLine("Массив 1: ");
-    foreach (string word in array_of_words_1)
-    {
-        count_words++;
-        count_symbols = word.Length;
-        if (count_symbols <= 3)
+    List<string> words = new List<string>();
+    Console.Write("[");
+    foreach (string word in arr)
+    {     
+        if (word.Length <= 3)
         {
-            Console.WriteLine($"[{word}]");
+            words.Add(word);
         }
     }
+    var stroka = string.Join(", ", words);
+    Console.Write(stroka + "]");
 }
 
+Console.Write("\n" + "[" + str_1 + "]" + " -> ");
 check_words_array(array_of_words_1);
+Console.Write("\n" + "[" + str_2 + "]" + " -> ");
+check_words_array(array_of_words_2);
+Console.Write("\n" + "[" + str_3 + "]" + " -> ");
+check_words_array(array_of_words_3);
 
