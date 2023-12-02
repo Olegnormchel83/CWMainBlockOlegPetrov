720e17f10717bbc68884c06ce26d2e21499a0b2f
Oleg OJ <cranberries2000@gmail.com>
2023-12-02 17:53:04 +0300

    Реализована функция check_words_array. Подробнее о ней - README.md - Пункт 2.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿System.Console.WriteLine("Hello world!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+string[] array_of_words_1 = {"Hello", "2", "world", ":-)"};
+string[] array_of_words_2 = {"1234", "1567", "-2", "computer science"};
+string[] array_of_words_3 = {"Russia", "Denmark", "Kazan"};
+
+void check_words_array(string[] arr)
+{
+    int count_words = 0, count_letters = 0;
+
+    Console.WriteLine("Массив 1: ");
+    foreach (string word in array_of_words_1)
+    {
+        count_words++;
+        count_letters = word.Length;
+        Console.WriteLine($"{word} - Количество символов: {count_letters}");
+    }
+
+Console.WriteLine($"Количество слов: {count_words}");
+}
+
+check_words_array(array_of_words_1);
